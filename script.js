93a1109cc425abf2546f5da330dd31de0a97b86e
sriramulagopi <129825285+sriramulagopi@users.noreply.github.com>
2023-12-09 12:41:07 +0530

    created a submission

--- script.js
+++ script.js
@@ -7,6 +7,6 @@ function datatypes(a,b){
     }
 }
 // do not change the code below.
-// let a = prompt("Enter Start of the Range.");
-// let b = prompt("Enter End Of the Range.");
+let a = prompt("Enter Start of the Range.");
+let b = prompt("Enter End Of the Range.");
 alert(datatypes(a,b));
