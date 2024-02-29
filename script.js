36acbd66925d479cdfb6fbf766327ce3ad9b7ea8
sriramulagopi <129825285+sriramulagopi@users.noreply.github.com>
2024-02-29 18:22:12 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,15 +1,16 @@
 function datatypes(a,b){
-	if((a==NaN || b==NaN)){
-		return '\'false\'';
+	if((a===NaN || b==NaN) || (a===NaN && b===NaN)){
+		return true;
 	}
-    else if ((typeof a) == (typeof b)){
+    else if ((typeof a) === (typeof b)){
         return true;
     }	
     else{
         return false;
     }
+	
 }
 // do not change the code below.
-// let a = prompt("Enter Start of the Range.");
-// let b = prompt("Enter End Of the Range.");
+let a = prompt("Enter Start of the Range.");
+let b = prompt("Enter End Of the Range.");
 alert(datatypes(a,b));
